636b2978c634dbf97ef349b05c6675a62129df34
Gerard Hynes <gkhynes@gmail.com>
2019-12-16 21:18:03 +0000

    Add Footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./footer.css"
+
+const Footer = () => (
+  <footer className="footer">
+    Built with
+    {` `}
+    <a href="https://www.gatsbyjs.org">Gatsby</a> {` `}by{` `}
+    <a href="https://github.com/GK-Hynes">Ger Hynes</a>
+  </footer>
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -24,25 +25,22 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a> {` `}by{` `}
-          <a href="https://github.com/GK-Hynes">Ger Hynes</a>
-        </footer>
+    <div className="container">
+      <div className="content">
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+        </div>
       </div>
-    </>
+      <Footer />
+    </div>
   )
 }
 
