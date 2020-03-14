de72ace4ccdabbc511e09265278da620bc8e2cba
Gerard Hynes <gkhynes@gmail.com>
2020-03-14 15:07:20 +0000

    Update pwa background colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#c53030`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
@@ -31,4 +31,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
