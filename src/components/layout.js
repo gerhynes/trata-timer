0f53c9643fafcdc10cbff76de69cff5cdb52d785
Gerard Hynes <gkhynes@gmail.com>
2020-10-08 22:12:52 +0100

    Replace default CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,31 +11,15 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+import "normalize.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <div className="container">
       <div className="content">
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <Header siteTitle={`Tráta Timer`} />
+        <div>
           <main>{children}</main>
         </div>
       </div>
